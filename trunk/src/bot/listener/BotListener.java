85c67e563f43bb3a65a24a37d7c8ddb0196f8a8d
ulrich_kroemer <ulrich_kroemer@4dfba36e-66fb-0310-a22b-af3a1b15f968>
2005-07-21 19:32:43 +0000

    updated comments

--- trunk/src/bot/listener/BotListener.java
+++ trunk/src/bot/listener/BotListener.java
@@ -5,8 +5,14 @@ import java.util.List;
 import bot.EventNotificationBot;
 
 /**
- * 
- * @author y2uk
+ * defines the common behaviour of a listener that may register itself with the
+ * bot. each listener has to provide all of the following methods in order to
+ * work properly. moreover as each class that registers itself with the bot has
+ * to implement this interface it is guaranteed that does methods exist.
+ * the default implementation for this interface is the BotListenerImpl class 
+ * (for each listener interface there exists an according default implemantation with
+ * Impl suffixed to the interface name)
+ * @author Ulrich Krömer
  *
  */
 public interface BotListener {
@@ -24,4 +30,19 @@ public interface BotListener {
 	 * @return the list of dependencies for this bot listener.
 	 */
 	public List<Class<? extends BotListener>> getDependencies();
+	
+	/**
+	 * is called after the feature is registered and initialized to provide 
+	 * the feature with the possibility to add some user defined initialization.
+	 * this should not be done in the constructor as not every information needed
+	 * may be accessible at that time.
+	 */
+	public void onLoad();
+	
+	/**
+	 * this method is called before a feature is unloaded.
+	 * this provides a feature with the possibilty to add some user defined behaviour
+	 * (like saving the current status) before the feature is unloaded.
+	 */
+	public void onUnload();
 }
